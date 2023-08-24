e5832cff54b4a1a241148deaa393eb8468f5bbc2
wagngomes gomes <wagner.gomes.cx2@gmail.com>
2023-08-24 00:50:10 +0000

    aula_1

---
+++ listaLivros.js
@@ -0,0 +1,27 @@
+const precos = [
+{
+    titulo: "Javascript",
+    preco: 25
+},
+{
+    titulo: "PHP",
+    preco: 15
+},
+{
+    titulo: "Java",
+    preco: 30
+},
+{
+    titulo: "Elixir",
+    preco: 50
+},
+{
+    titulo: "Golang",
+    preco: 45
+},
+{
+    titulo: "Python",
+    preco: 20
+},]
+
+module.exports = precos
